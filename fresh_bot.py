b3e09c29845ca727d5fbd0bb419f7a41ee190d76
nikitxskv <dmitnikita@ya.ru>
2016-10-02 00:54:11 +0300

    Bug fix5

--- fresh_bot.py
+++ fresh_bot.py
@@ -188,7 +188,9 @@ def get_song(song_index):
             url = songs[song_index - 1][1]
         song_name = wget.download(url)
         song_name = song_name.encode('utf-8')
-        song = open(song_name)
+        os.rename(song_name, "lol.mp3")
+        song = open("lol.mp3")
+        # song = open(song_name)
         return song
     except IOError:
         return None
